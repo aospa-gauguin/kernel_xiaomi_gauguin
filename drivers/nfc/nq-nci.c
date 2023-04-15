029f076f52928036f6cf542474e503c87e42a71e
ReallySnow <reallysnow233@gmail.com>
2023-04-15 15:00:15 +0800

    nfc: nq-nci: Import xiaomi changes
    Change-Id: If016e75f2790b4201f730e1a57d580fa0bd44fd5
    Signed-off-by: ReallySnow <reallysnow233@gmail.com>

--- drivers/nfc/nq-nci.c
+++ drivers/nfc/nq-nci.c
@@ -1,6 +1,7 @@
 // SPDX-License-Identifier: GPL-2.0-only
 /*
  * Copyright (c) 2015-2020, The Linux Foundation. All rights reserved.
+ * Copyright (C) 2021 XiaoMi, Inc.
  */
 
 #include <linux/kernel.h>
@@ -21,7 +22,9 @@
 #ifdef CONFIG_COMPAT
 #include <linux/compat.h>
 #endif
+#ifdef NQ_READ_INT
 #include <linux/jiffies.h>
+#endif
 #include <linux/regulator/consumer.h>
 
 struct nqx_platform_data {
@@ -1056,6 +1059,7 @@ static int nfcc_hw_check(struct i2c_client *client, struct nqx_dev *nqx_dev)
 	int ret = 0;
 
 	int gpio_retry_count = 0;
+	int send_retry_count = 0;
 	unsigned char reset_ntf_len = 0;
 	unsigned int enable_gpio = nqx_dev->en_gpio;
 	char *nci_reset_cmd = NULL;
@@ -1098,9 +1102,11 @@ static int nfcc_hw_check(struct i2c_client *client, struct nqx_dev *nqx_dev)
 
 reset_enable_gpio:
 	/* making sure that the NFCC starts in a clean state. */
+#ifdef NQ_READ_INT
 	gpio_set_value(enable_gpio, 1);/* HPD : Enable*/
 	/* hardware dependent delay */
 	usleep_range(10000, 10100);
+#endif
 	gpio_set_value(enable_gpio, 0);/* ULPM: Disable */
 	/* hardware dependent delay */
 	usleep_range(10000, 10100);
@@ -1118,10 +1124,23 @@ reset_enable_gpio:
 		dev_err(&client->dev,
 		"%s: - i2c_master_send core reset Error\n", __func__);
 
+		if (send_retry_count < MAX_RETRY_COUNT) {
+			send_retry_count  += 1;
+			goto reset_enable_gpio;
+		} else {
+			nqx_dev->nqx_info.info.chip_type = NFCC_SN100_A;
+			nqx_dev->nqx_info.info.rom_version = 0;
+			nqx_dev->nqx_info.info.fw_minor = 0;
+			nqx_dev->nqx_info.info.fw_major = 0;
+			goto err_nfcc_reset_failed;
+		}
+
 		if (gpio_is_valid(nqx_dev->firm_gpio)) {
 			gpio_set_value(nqx_dev->firm_gpio, 1);
 			usleep_range(10000, 10100);
 		}
+		dev_err(&client->dev,
+			"%s: - reset NFCC 2 - pull down and pull up VEN\n", __func__);
 		gpio_set_value(nqx_dev->en_gpio, 0);
 		usleep_range(10000, 10100);
 		gpio_set_value(nqx_dev->en_gpio, 1);
@@ -1166,12 +1185,15 @@ reset_enable_gpio:
 		}
 		goto err_nfcc_reset_failed;
 	}
-
+	/* hardware dependent delay */
+	msleep(60);
+#ifdef NQ_READ_INT
 	ret = is_data_available_for_read(nqx_dev);
 	if (ret <= 0) {
 		nqx_disable_irq(nqx_dev);
 		goto err_nfcc_hw_check;
 	}
+#endif
 
 	/* Read Response of RESET command */
 	ret = i2c_master_recv(client, nci_reset_rsp, NCI_RESET_RSP_LEN);
@@ -1184,11 +1206,16 @@ reset_enable_gpio:
 		goto err_nfcc_hw_check;
 	}
 
+	/* hardware dependent delay */
+	msleep(30);
+
+#ifdef NQ_READ_INT
 	ret = is_data_available_for_read(nqx_dev);
 	if (ret <= 0) {
 		nqx_disable_irq(nqx_dev);
 		goto err_nfcc_hw_check;
 	}
+#endif
 
 	/* Read Notification of RESET command */
 	ret = i2c_master_recv(client, nci_reset_ntf, NCI_RESET_NTF_LEN);
@@ -1237,11 +1264,11 @@ err_nfcc_reset_failed:
 		break;
 	case NFCC_SN100_A:
 	case NFCC_SN100_B:
-		dev_dbg(&client->dev,
+		dev_warn(&client->dev,
 		"%s: ## NFCC == SN100x ##\n", __func__);
 		break;
 	default:
-		dev_err(&client->dev,
+		dev_dbg(&client->dev,
 		"%s: - NFCC HW not Supported\n", __func__);
 		break;
 	}
