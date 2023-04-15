917b8d5de2cfd56be475f14d650fa6b910a05040
ReallySnow <reallysnow233@gmail.com>
2023-04-16 01:23:10 +0800

    pinctrl: Import xiaomi changes
    Change-Id: Ic30b5ecdbe6c06589b3420be7b41ed25858d706f

--- drivers/pinctrl/qcom/pinctrl-msm.c
+++ drivers/pinctrl/qcom/pinctrl-msm.c
@@ -1,5 +1,6 @@
 /*
  * Copyright (c) 2013, Sony Mobile Communications AB.
+ * Copyright (C) 2021 XiaoMi, Inc.
  * Copyright (c) 2013-2020, The Linux Foundation. All rights reserved.
  *
  * This program is free software; you can redistribute it and/or modify
